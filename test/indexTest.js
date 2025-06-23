535bd5a6c5acacb70a768e7f3012786b7765e413
erskinenyongesawanami <erskineviper@gmail.com>
2025-06-23 05:42:36 +0300

    add changes made

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,23 @@
-// The code below ensures that students who are using CodeGrade will get credit
-// for the code-along in Canvas; you can disregard it.
+const assert = require('assert');
 
-require("./helpers.js");
-
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
+describe('Article POST request', function () {
+  it('should add a new article and return it with an ID', async function () {
+    const response = await fetch("http://localhost:3000/articles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: "Fourth Article",
+        content: "This was added via fetch!"
+      })
     });
+
+    const data = await response.json();
+
+    // Assertion
+    assert.ok(data.id, "ID should be present in the response");
+    assert.strictEqual(data.title, "Fourth Article");
+    assert.strictEqual(data.content, "This was added via fetch!");
   });
 });
